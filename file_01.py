6a9bddaddfaa665538790b9bb08e573bdb4568ec
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-08 17:16:45 +0530

    if else conditonals | 12/08

--- file_01.py
+++ file_01.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+# # use os.environment to access enviornment variables
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','default_secret_key')
+
+posts = [
+    {
+        "author": "Menath Baddegana",
+        "title": "Blog Post 01",
+        "content": "First post content",
+        "datePosted": "April 21, 2022"
+    },
+    {
+        "author": "Amandi sadara",
+        "title": "Blog Post 02",
+        "content": "Second post content",
+        "datePosted": "November 76, 2023"
+    }
+]
+
+
 @app.route("/")
-def hello():
-    return "Hello, world!";
+@app.route("/home")
+def home():
+    return render_template("home.html",title = "Home", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title = "About")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
